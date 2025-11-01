1fb95731498502ab45a49218ed18c9d7b9bc5aa6
imkushalneupane <imkushalneupane@gmail.com>
2025-11-01 12:48:11 +0545

    New Input for Desktop

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,68 +1,79 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    public WeaponController weaponController;  //refrence to the WeaponController Scrpit
+    public WeaponController weaponController;  // reference to the WeaponController Script
 
     Vector2 moveDirection;
     Vector2 mousePosition;
 
-    [SerializeField]PlayerHealth _player;  //refrence to PlayerHealth script
-    Renderer _renderer; //refrence to renderer
+    [SerializeField] PlayerHealth _player;  // reference to PlayerHealth script
+    Renderer _renderer; // reference to renderer
 
-    
-
-
-
+    // Input System components
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction mousePositionAction;
 
     private void Start()
     {
         _player = GetComponent<PlayerHealth>();
         _player.OnPlayerDead += OnDied;
         _renderer = GetComponent<Renderer>();
-        
-        
+
+        // Initialize Input System
+        playerInput = GetComponent<PlayerInput>();
+        if (playerInput == null)
+        {
+            playerInput = gameObject.AddComponent<PlayerInput>();
+        }
+
+        // Get input actions
+        moveAction = playerInput.actions["Move"];
+        mousePositionAction = playerInput.actions["MousePosition"];
     }
 
     private void OnDied()
     {
         moveSpeed = 0f;
         _renderer.material.color = Color.grey;
+
+        // Disable input when player dies
+        if (playerInput != null)
+        {
+            playerInput.enabled = false;
+        }
     }
 
-
-
-
-
-    // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // Get movement input from new Input System
+        moveDirection = moveAction.ReadValue<Vector2>().normalized;
 
-        moveDirection = new Vector2(moveX, moveY).normalized; //assinging the moveDirection of the Player (up down left right)
-
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);  //position of mouse on screen
+        // Get mouse position from new Input System
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePositionAction.ReadValue<Vector2>());
     }
-
-   
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed; //Inplementing Actual Movement
-          
+        rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed; // Implementing Actual Movement
 
-        Vector2 aimDriection = mousePosition - rb.position; 
-        float aimAngle = Mathf.Atan2(aimDriection.y , aimDriection.x) * Mathf.Rad2Deg - 90f;
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
-    
-        
     }
 
-  
-
+    
+    private void OnDestroy()
+    {
+        if (_player != null)
+        {
+            _player.OnPlayerDead -= OnDied;
+        }
+    }
 }
