07180335d2fe0d99822ac2771a9a289e7972ee3c
imkushalneupane <imkushalneupane@gmail.com>
2025-11-05 22:27:08 +0545

    Added Ak and NitroBoost In BossScene

---
+++ Assets/Script/FinalBossBufferCash.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FinalBossBufferCash : MonoBehaviour
+{
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        WeaponController.HasAK47 = true;
+    }
+
+    
+}
