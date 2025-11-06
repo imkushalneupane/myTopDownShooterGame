5f2b58609489b305bf340eba232e32c88e40ada2
imkushalneupane <imkushalneupane@gmail.com>
2025-11-06 23:31:54 +0545

    Lost Progress

--- Assets/Script/SpikesControllerScript.cs
+++ Assets/Script/SpikesControllerScript.cs
@@ -10,6 +10,9 @@ public class SpikesControllerScript : MonoBehaviour
     [SerializeField]
     private Animator _spikeDown;
 
+    [SerializeField]
+    private AudioSource _spikesAudio;
+
     public void OnPressed()
     {
         StartCoroutine(OnPressurePlatePressed());
@@ -19,9 +22,13 @@ public class SpikesControllerScript : MonoBehaviour
     {
         _spikeUp.SetBool("PressureOn",true);
         _spikeDown.SetBool("PressureOn", true);
+        yield return new WaitForSeconds(.125f);
+        _spikesAudio.Play();
 
         yield return new WaitForSeconds(5f);
         _spikeUp.SetBool("PressureOn", false);
         _spikeDown.SetBool("PressureOn", false);
+        yield return new WaitForSeconds(.125f);
+        _spikesAudio.Play();
     }
 }
