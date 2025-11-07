c1e9166cc272ecf716c7599d6837a9e8d9ca734b
Jeevsan Shrestha <jeepitwitter@gmail.com>
2025-11-07 14:54:27 +0545

    playerperf set for nitropack

--- Assets/Script/RespawnPoint.cs
+++ Assets/Script/RespawnPoint.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Android;
+using UnityEngine.Playables;
 public class RespawnPoint : MonoBehaviour
 {
-    private Vector3 currentCheckpoint;
 
-    
-  
+    [SerializeField] NitroControllerScript _nitro;
+    private Vector3 currentCheckpoint;
+    private bool nitro = false;
+    private int i = 0;
+    [SerializeField] private GameObject NitroPack;
     void Start()
     {
 
-     transform.position = GetLocation();
-        
+        transform.position = GetLocation();
+        nitro = getnitropack();
+
+        if (nitro == true)
+        {
+            NitroPack.gameObject.SetActive(true);
+        }
+
+        i = getnitroCylinder();
+        for(int j = 0; j < i; j++)
+        {
+            _nitro.OnCylinderPick();
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -20,11 +34,44 @@ public class RespawnPoint : MonoBehaviour
             currentCheckpoint = transform.position;
         }
         SetLocation();
+        if (collision.CompareTag("NitroPack"))
+        {
+            nitro = true;
+        }
+        setnitropack();
+        
+        if( collision.CompareTag("NitroCylinder"))
+        {
+            i++;
+        }
+        setnitroCylinder();
+
+        
     }
 
+    private void setnitropack()
+    {
+        PlayerPrefs.SetInt("NitroPack", nitro ? 1 : 0);
+        PlayerPrefs.Save();
+
+    }
+    private bool getnitropack()
+    {
+        int value = PlayerPrefs.GetInt("NitroPack", 0);
+        return value == 1;
+    }
+    private void setnitroCylinder()
+    {
+        PlayerPrefs.SetInt("NitroCylinder", i);
+        PlayerPrefs.Save();
+    }
+    private int getnitroCylinder()
+    {
+        return PlayerPrefs.GetInt("NitroCylinder", 0);
+    }
     private void SetLocation()
     {
-        PlayerPrefs.SetFloat("X", currentCheckpoint.x );
+        PlayerPrefs.SetFloat("X", currentCheckpoint.x);
         PlayerPrefs.SetFloat("Y", currentCheckpoint.y);
         PlayerPrefs.SetFloat("Z", currentCheckpoint.z);
         PlayerPrefs.Save();
