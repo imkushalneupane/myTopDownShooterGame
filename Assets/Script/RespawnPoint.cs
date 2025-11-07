7b4817570dddaebb0ca9b824f453c9a82f20259a
imkushalneupane <imkushalneupane@gmail.com>
2025-11-07 14:24:49 +0545

    Respawn Mechanism

--- Assets/Script/RespawnPoint.cs
+++ Assets/Script/RespawnPoint.cs
@@ -1,33 +1,46 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Android;
 public class RespawnPoint : MonoBehaviour
 {
-    private Vector3 currentCheckpoint = new Vector3(-71, -48, 0);
+    private Vector3 currentCheckpoint;
 
-    public Transform spawnPoint;
-    private float respawnDelay = 3f;
-    private int i;
+    
   
     void Start()
     {
 
-     transform.position = currentCheckpoint;
-
+     transform.position = GetLocation();
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Checkpoint"))
+        if (collision.CompareTag("CheckPoint"))
         {
             currentCheckpoint = transform.position;
         }
+        SetLocation();
     }
 
-     IEnumerator RespawnAtCheckpoint()
-{
-    yield return new WaitForSeconds(respawnDelay);
-    transform.position = currentCheckpoint;
-}
+    private void SetLocation()
+    {
+        PlayerPrefs.SetFloat("X", currentCheckpoint.x );
+        PlayerPrefs.SetFloat("Y", currentCheckpoint.y);
+        PlayerPrefs.SetFloat("Z", currentCheckpoint.z);
+        PlayerPrefs.Save();
+    }  
+
+    private Vector3 GetLocation()
+    {
+        float x = PlayerPrefs.GetFloat("X" , -71);
+        float y = PlayerPrefs.GetFloat("Y", -48);
+        float z = PlayerPrefs.GetFloat("Z", 0); 
+
+
+      return new Vector3(x,y,z);
+    }
+
+   
 
 
 }
